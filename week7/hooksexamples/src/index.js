1b56067105694d97ac38d74777b8f9b0541be183
ashutoshc111 <ashutoshc2811@gmail.com>
2025-11-13 17:25:45 +0000

    week7

---
+++ week7/hooksexamples/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import EmojeeCounter from './EmojiCounter.js';
+import reportWebVitals from './reportWebVitals';
+import Hook_ControlledButtonState from './counter.js'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Hook_ControlledButtonState/>
+    <EmojeeCounter pic='Love'/>
+<EmojeeCounter pic='Sad'/>
+<EmojeeCounter pic='Like'/>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
